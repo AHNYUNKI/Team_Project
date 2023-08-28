b9b31315d30df806d65d4e614d029ec54d677acf
myukyu0828 <myukyu0828@gmail.com>
2023-08-28 23:14:07 +0900

    kmk0828

--- src/main/java/com/api/shop_project/controller/MainController.java
+++ src/main/java/com/api/shop_project/controller/MainController.java
@@ -1,2 +1,18 @@
-package com.api.shop_project.controller;public class MainController {
+package com.api.shop_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.GeneratedValue;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"","/"})
+    public String index(){
+        return index();
+    }
+
 }
