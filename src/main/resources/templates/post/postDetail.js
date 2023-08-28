561015a6181b93be97fd74e23350131d0738c74b
songwc3 <songwc3@gmail.com>
2023-08-28 17:49:43 +0900

    crud + 검색 + 페이징

--- src/main/resources/templates/post/postDetail.js
+++ src/main/resources/templates/post/postDetail.js
@@ -1,33 +1,50 @@
-const updateBtn = document.querySelector('#updateBtn');
-updateBtn.addEventListener('click', updateFn);
+// const updateBtn = document.querySelector('#updateBtn');
+// updateBtn.addEventListener('click', updateFn);
+//
+// function updateFn(e) {
+//     e.preventDefault();
+//
+//     const data = {
+//
+//         id: document.querySelector('#id').value,
+//         title: document.querySelector('#title').value,
+//         content: document.querySelector('#content').value,
+//         writer: document.querySelector('#writer').value,
+//     };
+//
+//     const url = "/post/postUpdate"
+//     const method = "POST";
+//
+//     fetch(url, {
+//         method: method,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }, body: JSON.stringify(data)
+//     }).then((response) => response.json())
+//         .then((data) => {
+//                 alert("회원수정 Success")
+//                 document.querySelector('#id').value = data.id;
+//                 document.querySelector('#title').value = data.title;
+//                 document.querySelector("#content").value = data.content;
+//                 document.querySelector("#writer").value = data.writer;
+//             }
+//         );
+//
+// }
 
-function updateFn(e) {
-    e.preventDefault();
-
-    const data = {
-
-        id: document.querySelector('#id').value,
-        title: document.querySelector('#title').value,
-        content: document.querySelector('#content').value,
-        writer: document.querySelector('#writer').value,
-    };
-
-    const url = "/post/postUpdate"
-    const method = "POST";
-
-    fetch(url, {
-        method: method,
-        headers: {
-            "Content-Type": "application/json"
-        }, body: JSON.stringify(data)
-    }).then((response) => response.json())
-        .then((data) => {
-                alert("회원수정 Success")
-                document.querySelector('#id').value = data.id;
-                document.querySelector('#title').value = data.title;
-                document.querySelector("#content").value = data.content;
-                document.querySelector("#writer").value = data.writer;
-            }
-        );
-
-}
+// document.getElementById("#updateBtn").addEventListener("click", function() {
+//     const form = document.createElement("form");
+//     form.method = "POST";
+//     form.action = "/post/postUpdate"; // POST 요청을 보낼 URL 설정
+//
+//     // Hidden input fields
+//     const input = document.createElement("input");
+//     input.type = "hidden";
+//     input.name = "parameterName"; // POST 요청에 포함될 파라미터 이름
+//     input.value = "parameterValue"; // 파라미터 값
+//
+//     form.appendChild(input);
+//     document.body.appendChild(form);
+//
+//     form.submit();
+// });
--- src/test/java/com/api/shop_project/PostTest.java
+++ src/test/java/com/api/shop_project/PostTest.java
@@ -2,7 +2,7 @@ package com.api.shop_project;
 
 import com.api.shop_project.domain.post.Post;
 import com.api.shop_project.repository.post.PostRepository;
-import com.api.shop_project.vo.post.PostVo;
+import com.api.shop_project.dto.post.PostVo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +16,7 @@ public class PostTest {
     @Autowired
     private PostRepository postRepository;
 
-
+    // 공지글 List
     @Test
     void postListTest(){
         List<PostVo> postVos = new ArrayList<>();
@@ -46,6 +46,59 @@ public class PostTest {
         }
 
 
+
+    }
+
+    // 자세히 보기
+    @Test
+    void PostDetailTest(){
+
+        Long id = 2L;
+
+        Post post =
+                postRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+
+        PostVo postVo = PostVo.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .writer(post.getWriter())
+                .createTime(post.getCreateTime())
+                .build();
+
+        System.out.println(postVo.getId());
+        System.out.println(postVo.getTitle());
+        System.out.println(postVo.getContent());
+        System.out.println(postVo.getWriter());
+        System.out.println(postVo.getCreateTime());
+
+
+    }
+
+    @Test
+    void PostUpdateTest(){
+
+//        postVo.setId(2L);
+//        postVo.setTitle("999");
+//        postVo.setContent("999");
+//        postVo.setWriter("999");
+
+        Post post = Post.builder()
+                .id(2L)
+                .title("999")
+                .content("999")
+                .writer("999")
+                .build();
+
+        postRepository.save(post);
+
+    }
+
+    @Test
+    void PostDeleteTest(){
+
+
+        postRepository.deleteById(2L);
 
     }
 
