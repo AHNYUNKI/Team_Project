967c1abc387773aad712c7a30605ad1d9fde8804
ahnyunki <ahnyunki@naver.com>
2023-09-18 17:42:55 +0900

    "상품 및 장바구니 수정 1차"

--- src/main/java/com/api/shop_project/controller/item/ItemController.java
+++ src/main/java/com/api/shop_project/controller/item/ItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -26,8 +27,8 @@ public class ItemController {
         return "/item/itemList";
     }
 
-    @GetMapping("/item/itemDetail")
-    public String getItem(Model model, @RequestParam Long itemId) {
+    @GetMapping("/item/itemDetails/{itemId}")
+    public String getItem(Model model, @PathVariable("itemId") Long itemId) {
         ItemResponse item = itemService.getItem(itemId);
 
         model.addAttribute("item", item);
--- src/main/java/com/api/shop_project/repository/cart/CartRepositoryCustom.java
+++ src/main/java/com/api/shop_project/repository/cart/CartRepositoryCustom.java
@@ -1,11 +1,11 @@
 package com.api.shop_project.repository.cart;
 
-import com.api.shop_project.domain.cart.CartItem;
 import com.api.shop_project.dto.response.cart.CartFindOne;
+import com.api.shop_project.dto.response.cart.CartResponse;
 
 import java.util.List;
 
 public interface CartRepositoryCustom{
 
-    List<CartItem> cartFindOne(CartFindOne cartFindOne);
+    List<CartResponse> cartFindOne(CartFindOne cartFindOne);
 }
